af1009f90c55bd4ab518451b566988c99ef53339
Eirik Vale Aase <e.v.aase@gmail.com>
2018-03-09 19:31:58 +0100

    Plot normal force, clean up code, etc etc

--- LabTheAnswer.py
+++ LabTheAnswer.py
@@ -3,17 +3,16 @@ import matplotlib
 from matplotlib import pyplot as plt
 import csv
 import pandas as pd
+
 # Input a tab seperated list of (t, x, y) values.
 # Produce coefficients corresponding to a function of degree 15.
 def iptrack(filename):
     data = np.loadtxt(filename, skiprows=2)
     polyfit = np.polyfit(data[:, 1], data[:, 2], 15)
-    print(polyfit)
     return polyfit
 
-
 # Input coefficients from iptrack.
-# Return a list of [y-coordinates, velocities, accelerations, alphas, radius']
+# Return a list of [y-coordinates, velocities, accelerations, alphas, radius]
 def trvalues(p, x):
     y = np.polyval(p, x)
     dp = np.polyder(p)
@@ -24,11 +23,8 @@ def trvalues(p, x):
     R = (1.0 + dydx ** 2) ** 1.5 / d2ydx2
     return [y, dydx, d2ydx2, alpha, R]
 
-
 data = pd.read_csv('M1Data.txt', delimiter='\t', skiprows=1)
 data = data.sort_values('t')
-
-print(data)
 
 # Constants
 m = 0.0027  # Mass of ball
@@ -39,47 +35,27 @@ end_x = 2.0 # The end position in x-axis
 num_points = 10000 # Number of points
 h = (data['t'].iloc[-1]-data['t'].iloc[0])/num_points # Stepsize
 
-
-
 # Find the coefficients of a polynomial of degree 15 from the (t, x, y) values.
 coefficients = iptrack("data_tabs.txt")
-
-# Domain space from 0 to 1.20, 10 000 points for precision.
-# x = np.linspace(start_x, end_x, num_points)
-
-# Values are a list of [y(x), dy/dx, d^2y/d^2x, alphas, radius']
-#values = trvalues(coefficients, x)
-
-# Acceleration values of the rolling ball.
-#acceleration_values = values[2]
-
-# alpha_values are the angles with respect to the horizontal plane.
-#alpha_values = values[3]
-
-# The alpha_values transformed by the function sin(x).
-#sinus_alpha_values = np.sin(alpha_values)
-
-#plt.subplot(311)
-# This will plot the friction force with respect to x.
-#plt.plot(x, m * g * sinus_alpha_values - m * acceleration_values)
 
 gca = plt.gca()
 gca.set_ylim([-0.01, 0.05])
 gca.yaxis.set_major_locator(matplotlib.ticker.MultipleLocator(0.005))
 gca.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(0.05))
 
-
 # This is a implemented eulers method
 s_now = 0
 v_now = 0
 x_now = 0
 y_now = data['y'].iloc[0]
-s = []
-v = []
-x = []
-y = []
-a = []
-alpha_list = []
+s = []                     # List of positions traveled in x direction
+v = []                     # List of velocity values
+x = []                     # List of x values
+y = []                     # List of y(x) values
+a = []                     # List of accelerations
+alpha_list = []            # List of alpha values (angles)
+n = []                     # List of normal force values
+
 for i in range(num_points):
     if (x_now > 1.21): break
     alpha = trvalues(coefficients, x_now)[3]
@@ -89,6 +65,7 @@ for i in range(num_points):
     x_next = x_now + (s_next - s_now)*np.cos(alpha)
     y_next = y_now - (s_next - s_now)*np.sin(alpha)
 
+    n.append(np.cos(alpha) * m * g)
     alpha_list.append(alpha)
     a.append(a_next)
     x.append(x_next)
@@ -101,30 +78,38 @@ for i in range(num_points):
     s_now = s_next
     v_now = v_next
 
-
-plt.subplot(221)
-plt.plot(x, s, label='S(x)')
-plt.legend()
-
-plt.subplot(222)
-plt.plot(x, v, label='v(x)')
-plt.legend()
-
-plt.subplot(223)
-plt.plot(x, y, label='y(x)')
+plt.subplot(231)
+plt.plot(x, s)
+plt.ylabel("posisjon x [m/s]")
+plt.xlabel("tid t [s]")
 data_cut = data[data['x']<=1.2]
 y_interpolant = trvalues(coefficients, data_cut['x'])[0]
 
-plt.plot(data_cut['x'].iloc[0:201], data_cut['y'], label='raw_data')
-plt.plot(data_cut['x'].iloc[0:201], y_interpolant, label='Y_interpolant')
+plt.subplot(232)
+plt.plot(x, v)
+plt.ylabel("hastighet v [m/s]")
+plt.xlabel("tid t [s]")
 plt.legend()
 
-plt.subplot(224)
-f = np.subtract(0.03*(9.82*np.sin(alpha_list)), a)
-plt.plot(x, a, label='a(x)')
-plt.plot(x, f, label='f(x)')
+plt.subplot(233)
+plt.plot(s, a)
+plt.ylabel("akselerasjon a [m/s^2]")
+plt.xlabel("posisjon x [m]")
 plt.legend()
 plt.plot()
 
+plt.subplot(234)
+plt.plot(x, n)
+plt.ylabel("normalkraft N [mN]")
+plt.xlabel("posisjon x [m]")
+plt.legend()
+
+plt.subplot(235)
+f = np.subtract(0.03*(9.82*np.sin(alpha_list)), a)
+plt.plot(s, f)
+plt.ylabel("friksjonskraft f [N]")
+plt.xlabel("posisjon x [m]")
+plt.legend()
+plt.plot()
 
 plt.show()
