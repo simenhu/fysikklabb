e9f864526464c87b62eff815fcfcb2e4f3562586
MKohm <marius.kohmann@gmail.com>
2018-04-17 12:58:32 +0200

    fix bug related to plotting position vs velocity

--- LabTheAnswer.py
+++ LabTheAnswer.py
@@ -7,23 +7,24 @@ SMALL_SIZE = 9
 MEDIUM_SIZE = 30
 BIGGER_SIZE = 30
 
-plt.rc('font', size=BIGGER_SIZE)          # controls default text sizes
-plt.rc('axes', titlesize=BIGGER_SIZE)     # fontsize of the axes title
-plt.rc('axes', labelsize=BIGGER_SIZE)    # fontsize of the x and y labels
-plt.rc('xtick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
-plt.rc('ytick', labelsize=MEDIUM_SIZE)    # fontsize of the tick labels
-plt.rc('legend', fontsize=BIGGER_SIZE)    # legend fontsize
+plt.rc('font', size=BIGGER_SIZE)  # controls default text sizes
+plt.rc('axes', titlesize=BIGGER_SIZE)  # fontsize of the axes title
+plt.rc('axes', labelsize=BIGGER_SIZE)  # fontsize of the x and y labels
+plt.rc('xtick', labelsize=MEDIUM_SIZE)  # fontsize of the tick labels
+plt.rc('ytick', labelsize=MEDIUM_SIZE)  # fontsize of the tick labels
+plt.rc('legend', fontsize=BIGGER_SIZE)  # legend fontsize
 plt.rc('figure', titlesize=BIGGER_SIZE)  # fontsize of the figure title
-left  = 0.09  # the left side of the subplots of the figure
-right = 0.98    # the right side of the subplots of the figure
-bottom = 0.09   # the bottom of the subplots of the figure
-top = 0.97      # the top of the subplots of the figure
-wspace = 0.21   # the amount of width reserved for space between subplots,
-               # expressed as a fraction of the average axis width
-hspace = 0.31   # the amount of height reserved for space between subplots,
-               # expressed as a fraction of the average axis height
+left = 0.09  # the left side of the subplots of the figure
+right = 0.98  # the right side of the subplots of the figure
+bottom = 0.09  # the bottom of the subplots of the figure
+top = 0.97  # the top of the subplots of the figure
+wspace = 0.21  # the amount of width reserved for space between subplots,
+# expressed as a fraction of the average axis width
+hspace = 0.31  # the amount of height reserved for space between subplots,
+# expressed as a fraction of the average axis height
 plt.subplots_adjust(left, bottom, right, top,
-                wspace, hspace)
+                    wspace, hspace)
+
 
 # Input a tab seperated list of (t, x, y) values.
 # Produce coefficients corresponding to a function of degree 15.
@@ -50,8 +51,8 @@ dataFile = "M2Data.txt"
 data = pd.read_csv('%s' % dataFile, delimiter='\t', skiprows=1)
 data = data.sort_values('t')
 
-dataFile2 = "M3Velocity.txt"
-data2 = pd.read_csv('%s' % dataFile, delimiter='\t', skiprows=1)
+dataFile2 = "M2DataWithVelocity.txt"
+data2 = pd.read_csv('%s' % dataFile2, delimiter='\t', skiprows=1)
 data2 = data2.sort_values('t')
 
 # Constants
@@ -60,7 +61,7 @@ g = 9.82  # Constant of gravitation
 c = 2 / 3
 start_x = 0.0  # The start position in x-axis
 end_x = 2.0  # The end position in x-axis
-num_points = 1000  # Number of points
+num_points = 196  # Number of points
 h = data['t'].iloc[-1] / num_points  # Time step size
 
 # Find the coefficients of a polynomial of degree 15 from the (t, x, y) values.
@@ -73,15 +74,15 @@ gca.xaxis.set_major_locator(matplotlib.ticker.MultipleLocator(0.05))
 
 # This is a implemented eulers method
 position_now = 0
-velocity_now = 0 # Position the traveled direction
+velocity_now = 0  # Position the traveled direction
 time_now = 0
 y_now = data['y'].iloc[0]
 x_now = data['x'].iloc[0]
-position = [] # List of positions traveled in the track
-velocity = [] # List of velocity values
-time = np.linspace(0, data['t'].iloc[0], num_points)  # list of time samples to use when plotting by time
-y = [] # List of y(x) values
-x = [] # LIst of x values
+position = []  # List of positions traveled in the track
+velocity = []  # List of velocity values
+time = np.linspace(0, data['t'].iloc[-1], num_points)  # list of time samples to use when plotting by time
+y = []  # List of y(x) values
+x = []  # LIst of x values
 acceleration = []  # List of accelerations
 alpha_list = []  # List of alpha values (angles)
 n = []  # List of normal force values
@@ -99,7 +100,6 @@ for i in range(num_points):
     n.append(np.cos(alpha) * m * g)
     alpha_list.append(alpha)
     acceleration.append(acceleration_next)
-    # time.append(time_next)
     y.append(y_next)
     x.append(x_next)
     position.append(position_next)
@@ -114,34 +114,58 @@ for i in range(num_points):
 plt.style.use("bmh")
 
 
-def plotXandTExperimental():
+def plotExperimentalPosition():
     plt.subplot(211)
     plt.plot(data['t'], data['y'], label="Eksperimentell")
-    gca.set_ylim([0.4, 0.7])
-
-    plt.legend()
-
-
-def plotNumericalPosition():
-    global gca
-    plt.plot(time, y, label="Numerisk")
-    plt.ylabel("posisjon x [m/s]")
+    plt.ylabel("posisjon y [m]")
     plt.xlabel("tid t [s]")
     plt.legend()
     gca = plt.gca()
     gca.set_xlim([0, 1.5])
-    gca.set_ylim([0.4, 0.7])
+
+
+def plotNumericalPosition():
+    global gca
+    plt.subplot(211)
+    plt.plot(time, y, label="Numerisk")
+    plt.ylabel("posisjon x [m]")
+    plt.xlabel("tid t [s]")
+    plt.legend()
+    gca = plt.gca()
+    gca.set_xlim([0, 1.5])
+    gca.set_ylim([0.45, 0.65])
+
+
+def plotExperimentalVelocity():
+    plt.subplot(212)
+
+    plt.plot(data2['t'], data2['v'], label="Eksperimentell")
+    plt.ylabel("hastighet v [m/s]")
+    plt.xlabel("tid t [s]")
+    plt.legend()
+    gca = plt.gca()
+    gca.set_xlim([0, 1.5])
+    gca.set_ylim([0, 2])
+
+
+def plotNumericalVelocity():
+    plt.subplot(212)
+    print(time)
+    plt.plot(time, velocity, label="Numerisk")
+    plt.ylabel("hastighet v [m/s]")
+    plt.xlabel("tid t [s]")
+    plt.legend()
 
 
 def plotNumericalAcceleration():
     global gca
     plt.subplot(211)
     plt.plot(x, acceleration, label="Numerisk")
-    plt.ylabel("akselerasjon a [m/s^2]")
+    plt.ylabel("akselerasjon a [m/s²]")
     plt.legend()
-    plt.xlabel("position x [m]")
+    plt.xlabel("posisjon x [m]")
     gca = plt.gca()
-    gca.set_xlim([0,1.371])
+    gca.set_xlim([0, 1.371])
     plt.axvline(x=0.48, color='r', linestyle='dashed')
     plt.axvline(x=1.016, color='r', linestyle='dashed')
 
@@ -158,7 +182,7 @@ def plotFandN():
     gca.set_xlim([0, 1.371])
     gca.set_ylim([0.019, 0.027])
     plt.axvline(x=0.48, color='r', linestyle='dashed')
-    plt.axvline(x=1.016, color='r',linestyle='dashed')
+    plt.axvline(x=1.016, color='r', linestyle='dashed')
 
     # This plots the friction force dependent on x
     ax2 = plt.subplot(212, sharex=ax1)
@@ -170,6 +194,7 @@ def plotFandN():
     plt.axvline(x=0.48, color='r', linestyle='dashed')
     plt.axvline(x=1.016, color='r', linestyle='dashed')
 
+
 """
     ax3 = plt.subplot(313, sharex = ax1)
     plt.plot(x, y)
@@ -180,31 +205,6 @@ def plotFandN():
     plt.axvline(x=0.48, color='r', linestyle='dashed')
     plt.axvline(x=1.016, color='r', linestyle='dashed')
 """
-
-
-
-
-def plotExperimentalVelocity():
-    global gca
-    plt.subplot(212)
-
-    # Data2['x'] is actually Data2['v']
-    plt.plot(data2['t'], data2['x'], label="Eksperimentell")
-    plt.ylabel("Hastighet v [m/s]")
-    plt.legend()
-    plt.xlabel("tid t [s]")
-
-
-def plotNumericalVelocity():
-    global gca
-    plt.subplot(212)
-    plt.plot(time, velocity, label="Numerisk")
-    plt.legend()
-    plt.ylabel("hastighet v [m/s]")
-    plt.xlabel("tid t [s]")
-    gca = plt.gca()
-
-    gca.set_xlim([0, 1.5])
 
 
 def plotF():
@@ -241,8 +241,9 @@ def plotInterpolatedCurveWithExperimentalCurve():
     plt.legend()
 
 
-plotNumericalAcceleration()
-plotF()
+plotNumericalPosition()
+plotExperimentalPosition()
 
-
+plotNumericalVelocity()
+plotExperimentalVelocity()
 plt.show()
